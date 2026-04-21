ef4422f1eb808a0b282b4f7d3649b4cdf2f74b77
skimpsave <skimpsave@gmail.com>
2026-04-21 10:16:44 -0400

    Update model_hr.py

--- model_hr.py
+++ model_hr.py
@@ -1,1 +1,22 @@
 
+import pandas as pd
+from sklearn.ensemble import GradientBoostingRegressor
+
+def train_hr_model(df):
+
+    features = [
+        "ISO", "HardHit", "Barrel%", "FB%", 
+        "HR/9", "ParkFactor", "pitcher_weakness"
+    ]
+
+    df = df.dropna()
+
+    X = df[features]
+    y = df["HR"]  # historical HRs
+
+    model = GradientBoostingRegressor()
+    model.fit(X, y)
+
+    df["HR_PROB"] = model.predict(X)
+
+    return df, model
