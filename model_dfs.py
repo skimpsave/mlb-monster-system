7c9b47550cd189f64b3b3d409c29ab5e1fe23a82
skimpsave <skimpsave@gmail.com>
2026-04-21 10:16:18 -0400

    Update model_dfs.py

--- model_dfs.py
+++ model_dfs.py
@@ -1,1 +1,12 @@
 
+def calculate_dfs_points(df):
+
+    # DraftKings scoring example
+    df["DFS_PROJ"] = (
+        df["HR_PROB"] * 10 +
+        df["split_wOBA"] * 8 +
+        df["HardHit"] * 3 +
+        df["order_score"] * 2
+    )
+
+    return df
