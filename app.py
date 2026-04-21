307bf3c5042d9e963d70e62f425b3407c268d5e6
skimpsave <skimpsave@gmail.com>
2026-04-21 10:09:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import streamlit as st
 import pandas as pd
 
 st.title("🔥 MLB MONSTER SYSTEM")
-
-DFS_URL = "https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=0#gid=0"
-HR_URL = "https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=0#gid=0"
-BETS_URL = "https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=0#gid=0"
-STACKS_URL = "https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=0#gid=0"
+DFS_URL = https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=0#gid=0"
+HR_URL = "https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=2074315006#gid=2074315006"
+BETS_URL = "https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=52174597#gid=52174597"
+STACKS_URL = "https://docs.google.com/spreadsheets/d/1LQmI3YBA_oOVQUSXiWsN2JbtP2mgf5YNOCEof0MUFrY/edit?gid=752175886#gid=752175886"
 
 def load(url):
-    return pd.read_csv(url)
+    return pd.read_csv()
 
 dfs = load(DFS_URL)
 hr = load(HR_URL)
